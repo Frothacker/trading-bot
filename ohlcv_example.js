154a2303030d431035ad9f2ae791b7d026ce6bdb
Finn <finn.rothacker@agridigital.io>
2019-03-07 20:53:43 +1100

    start outputting files to json

--- ohlcv_example.js
+++ ohlcv_example.js
@@ -4,6 +4,7 @@ const ccxt = require("ccxt");
 const asciichart = require("asciichart");
 const asTable = require("as-table");
 const log = require("ololog").configure({ locate: false });
+const fs = require("fs-extra");
 
 require("ansicolor").nice;
 
@@ -21,11 +22,22 @@ require("ansicolor").nice;
     const lastPrice = ohlcv[ohlcv.length - 1][index]; // closing price
     let series = ohlcv.map(x => x[index]); // closing price
 
+    // output to a file
+    const filePath = "./closingValues.json";
+    fs.ensureFileSync(filePath);
+    const timestampIndex = 0;
+    const output = { indexKey: series }; ///// currently Cannot get this index key to hold a timestamp. It just interprets 'indexKey' as a string;
+
+    fs.writeFileSync(filePath, JSON.stringify(output));
+    console.log(`\nClosing values written to ${filePath}\n`);
+
+    // Format for chart
     // If series is too long, only present the 100 most recent closing prices on the chart. Else chart become un-readable in terminal due to too many data points in the terminal. Remove line below to chart the full output from 1m and 3m candles.
     series.length > 100
         ? (series = series.slice(series.length - 100, series.length - 1))
         : "";
 
+    // Create chart
     const bitcoinRate = ("₿ = $" + lastPrice).green;
     const chart = asciichart.plot(series, {
         height: 50,
